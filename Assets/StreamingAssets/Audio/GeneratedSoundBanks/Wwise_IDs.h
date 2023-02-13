b981acfe2928dde364a9ec4da7547ba67bc56ca0
KhaoKG <Evanseamans@gmail.com>
2023-02-13 03:26:58 -0800

    Start Wwise Project
    Integrate Wwise
    Appended Gitignore
    Started Project

---
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID GUNRELOAD = 323245414U;
+        static const AkUniqueID GUNSHOOT = 3194616450U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace WEAPON_SWITCH
+        {
+            static const AkUniqueID GROUP = 283915908U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID GUNREVOLVER = 2548468192U;
+                static const AkUniqueID GUNRIFLE = 163030769U;
+                static const AkUniqueID GUNSHOTGUN = 831777997U;
+                static const AkUniqueID GUNSNIPER = 998971168U;
+            } // namespace SWITCH
+        } // namespace WEAPON_SWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID MUSICVOLUME = 2346531308U;
+        static const AkUniqueID RELOADLENGTH = 4259737718U;
+        static const AkUniqueID SFXVOLUME = 988953028U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID GAMEPLAY = 89505537U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
