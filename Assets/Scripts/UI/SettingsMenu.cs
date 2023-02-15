de377fb083ccd65d70161367922c981f0216977d
The Crzy Potato <AydenAA@icloud.com>
2023-02-14 21:51:33 -0800

    Version 1 Main Menu
    Finished the first prototype of the main menu for the game. This will be improved as I get the art I need. I also still need to add remaining credits that I have no received yet. Again, this is not final, but it works.
    
    Note: In the Main Menu Container, you can set what the first level should be when the player presses the "Play" button ;)

---
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    [SerializeField] private TMP_Dropdown resolutionDropdown;
+    private Resolution[] _resolutions;
+    private List<Resolution> _filteredResolutions;
+
+    private int _currentRefreshRate;
+    private int _currentResolutionIndex = 0;
+
+    [Tooltip("Will toggle visibility of debug.logs")]
+    [SerializeField] private bool debugMode;
+    
+    private void Awake()
+    {
+        if (gameObject.activeSelf)
+            gameObject.SetActive(false); // Disabled by default, in case left active prior to playmode
+    }
+
+    private void Start()
+    {
+        _resolutions = Screen.resolutions;
+        _filteredResolutions = new List<Resolution>();
+        
+        resolutionDropdown.ClearOptions();
+        _currentRefreshRate = Screen.currentResolution.refreshRate;
+
+        if (debugMode) Debug.Log("RefreshRate: " + _currentRefreshRate);
+        
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            if (debugMode) Debug.Log("Resolution: " + _resolutions[i]);
+            if (_resolutions[i].refreshRate == _currentRefreshRate)
+            {
+                _filteredResolutions.Add(_resolutions[i]);
+            }
+        }
+
+        List<string> options = new List<string>();
+        for (int i = 0; i < _filteredResolutions.Count; i++)
+        {
+            string resolutionOption = _filteredResolutions[i].width + "x" + _filteredResolutions[i].height + " " + _filteredResolutions[i].refreshRate + " Hz";
+            options.Add(resolutionOption);
+            if (_filteredResolutions[i].width == Screen.width && _filteredResolutions[i].height == Screen.height)
+            {
+                _currentResolutionIndex = i;
+            }
+        }
+        
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = Mathf.Max(options.Count);
+        resolutionDropdown.RefreshShownValue();
+    }
+
+    #region Video
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution resolution = _filteredResolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, true);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Debug.Log("Fullscreen: " + isFullScreen);
+        Screen.fullScreen = isFullScreen;
+    }
+
+    #endregion
+
+    #region Audio
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+    #endregion
+    
+    public void CloseSettingsMenu()
+    {
+        gameObject.SetActive(false);
+    }
+}
