b149931bf5d9175a074d3dd037cf4e4ecc577d0a
zsaviourch <zsaviour@character.xyz>
2023-02-16 21:47:33 +0530

    Added simple navigation mesh for the levels

---
+++ Assets/Scripts/AI/Grid.cs
@@ -0,0 +1,125 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public bool isWalkable;
+    public Vector2 worldPosition;
+    public int gridX;
+    public int gridY;
+
+    public Node(bool isWalkable, Vector2 worldPosition, int gridX, int gridY)
+    {
+        this.isWalkable = isWalkable;
+        this.worldPosition = worldPosition;
+        this.gridX = gridX;
+        this.gridY = gridY;
+    }
+}
+
+
+public class Grid : MonoBehaviour
+{
+    public bool drawDebug; // flag to enable/disable debug drawing
+    public LayerMask obstacleMask;
+    public Vector2 gridWorldSize;
+    public float nodeRadius;
+    public Node[,] grid;
+
+    float nodeDiameter;
+    int gridSizeX, gridSizeY;
+
+    void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridWorldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridWorldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    void Update()
+    {
+        DrawDebug();
+    }
+
+    void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector2 worldBottomLeft = (Vector2)transform.position - Vector2.right * gridWorldSize.x / 2 - Vector2.up * gridWorldSize.y / 2;
+
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Vector2 worldPoint = worldBottomLeft + Vector2.right * (x * nodeDiameter + nodeRadius) + Vector2.up * (y * nodeDiameter + nodeRadius);
+                bool isWalkable = !Physics2D.OverlapCircle(worldPoint, nodeRadius, obstacleMask);
+                grid[x, y] = new Node(isWalkable, worldPoint, x, y);
+            }
+        }
+    }
+
+    public Node NodeFromWorldPoint(Vector2 worldPosition)
+    {
+        float percentX = (worldPosition.x + gridWorldSize.x / 2) / gridWorldSize.x;
+        float percentY = (worldPosition.y + gridWorldSize.y / 2) / gridWorldSize.y;
+        percentX = Mathf.Clamp01(percentX);
+        percentY = Mathf.Clamp01(percentY);
+
+        int x = Mathf.RoundToInt((gridSizeX - 1) * percentX);
+        int y = Mathf.RoundToInt((gridSizeY - 1) * percentY);
+        return grid[x, y];
+    }
+
+    public List<Node> GetNeighbours(Node node)
+    {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                if (x == 0 && y == 0)
+                    continue;
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+
+        return neighbours;
+    }
+
+void DrawDebug()
+{
+    if (!drawDebug) return;
+
+    if (grid != null)
+    {
+        for (int x = 0; x < gridSizeX; x++)
+        {
+            for (int y = 0; y < gridSizeY; y++)
+            {
+                Node node = grid[x, y];
+                if (node != null)
+                {
+                    Vector2 horizontalOffset = new Vector2(nodeRadius, 0f);
+                    Vector2 verticalOffset = new Vector2(0f, nodeRadius);
+                    Debug.DrawRay(node.worldPosition - horizontalOffset, horizontalOffset * 2f,node.isWalkable ? Color.green : Color.red);
+                    Debug.DrawRay(node.worldPosition - verticalOffset, verticalOffset * 2f, node.isWalkable ? Color.green : Color.red);
+                }
+            }
+        }
+    }
+}
+
+}
+
+
+
+
+
