ef228b817471b5d8593383395050cb3926aeeca4
The Crzy Potato <AydenAA@icloud.com>
2023-02-16 17:29:32 -0800

    Finished Pause menu and In game HUD,
    These are of course the first versions that will be improved.

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject settingsMenu;
+    [SerializeField] private GameObject pauseMenuContainer;
+
+    public bool gameIsPaused; // Any other scripts that need to know if the game is paused can reference this bool from this script
+    
+    void Start()
+    {
+        pauseMenuContainer.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    private void PauseGame()
+    {
+        pauseMenuContainer.SetActive(true);
+        Time.timeScale = 0f; // Time is now paused
+        gameIsPaused = true;
+        Debug.Log("Game is Paused");
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuContainer.SetActive(false);
+        Time.timeScale = 1f; // Time is now back at normal scale
+        gameIsPaused = false;
+        Debug.Log("Game is Unpaused");
+    }
+    
+    public void ToggleSettingsMenu()
+    {
+        if (!settingsMenu.gameObject.activeSelf)
+        {
+            settingsMenu.gameObject.SetActive(true);
+        }
+        else
+        {
+            settingsMenu.gameObject.SetActive(false);
+        }
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quitting Game...");
+    }
+}
