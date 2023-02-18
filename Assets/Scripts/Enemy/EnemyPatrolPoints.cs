4076a1a8bee1be778554f0efa76fe8d71d43f2d2
zsaviourch <zsaviour@character.xyz>
2023-02-18 18:32:34 +0530

    Added EnemyPatrolPoints system

--- Assets/Scripts/Enemy/EnemyPatrolPoints.cs
+++ Assets/Scripts/Enemy/EnemyPatrolPoints.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyPatrol : MonoBehaviour
+public class EnemyPatrolPoints : MonoBehaviour
 {
     [System.Serializable]
     public enum EnemyType
