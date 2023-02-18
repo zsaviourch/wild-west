e8c9660a18fe6a2741b890163e7c677543534131
The Crzy Potato <AydenAA@icloud.com>
2023-02-17 16:01:15 -0800

    Finished version one of the map and level loading
    I plan on making the level loader prefab not destroy on load or something.

--- Assets/Scripts/UI/LevelLoader.cs
+++ Assets/Scripts/UI/LevelLoader.cs
@@ -1,29 +1,105 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
+
+// To Do: Let the Level loader remember what city it got to previously to then choose the next one it hasn't been to
 
 public class LevelLoader : MonoBehaviour
 {
-    public Animator transition;
+    public GameObject mapCanvas; // This is essentially the loading screen
+    public Transform horseIcon;
+    public float horseIconSpeed = 100f;
+    public Animator crossfade;
 
-    [SerializeField] private float transitionTime = 1f;
+    public List<Transform> cities;
+    [SerializeField] private Transform currentLocation;
+    [SerializeField] private Transform destination;
 
-    public void LoadNextLevel()
+    [SerializeField] private float transitionTime = 1f; // Should be the same amount of time it takes for the crossfade animation to complete
+    public GameObject readyButton;
+    private bool _readyButtonPressed;
+    private bool canHorseMove;
+
+    public bool HorseArrivedAtDestination()
     {
-        StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+        if (horseIcon.position == destination.position)
+        {
+            Debug.Log("Horse Icon has Arrived at destination!");
+            return true;
+        }
+        else
+            return false;
+    }
+    
+    private bool ReadyToContinue() // When play clicks the button that appears on the screen when prompted, they will load into the next level
+    {
+        if (_readyButtonPressed)
+            return true;
+        else
+            return false;
+    }
+    
+    private void Start()
+    {
+        readyButton.SetActive(false);
+        horseIcon.position = currentLocation.position; // I'm going to use this line when we load into the next scene so the horse icon knows where to start for the next map transition
+        mapCanvas.SetActive(false);
+        canHorseMove = false;
     }
 
-    IEnumerator LoadLevel(int levelIndex)
+    private void Update()
+    {
+        if (canHorseMove)
+            MoveHorseIcon();
+    }
+
+    public void LoadScene(int sceneId)
+    {
+        StartCoroutine(LoadLevel(sceneId));
+    }
+
+    public void PlayerReadyToggle()
+    {
+        _readyButtonPressed = true;
+    }
+
+    private void MoveHorseIcon()
+    {
+        horseIcon.position = Vector3.MoveTowards(horseIcon.position, destination.position, Time.deltaTime * horseIconSpeed);
+    }
+    
+    IEnumerator LoadLevel(int sceneId) // sceneID is the Level's build order index
     {
         // Play Transition Animation
-        transition.SetTrigger("Start");
+        crossfade.SetTrigger("Start");
         
-        // Wait
+        yield return new WaitForSeconds(transitionTime);
+        
+        mapCanvas.SetActive(true);
+        crossfade.SetTrigger("End");
+        
         yield return new WaitForSeconds(transitionTime);
 
+        canHorseMove = true;
+
+        // Wait until the horse has arrived at the next city
+        yield return new WaitUntil(HorseArrivedAtDestination);
+
+        // Enable continue button & wait for player to press continue button
+        readyButton.SetActive(true);
+        yield return new WaitUntil(ReadyToContinue);
+        
+        crossfade.SetTrigger("Start");
+        
+        yield return new WaitForSeconds(transitionTime);
+
+        currentLocation = destination; // Our new current location is now what was our destination
+        
         // Load Scene
-        SceneManager.LoadScene(levelIndex);
-        // transition.SetTrigger("End");
+        SceneManager.LoadScene(sceneId);
     }
 }
